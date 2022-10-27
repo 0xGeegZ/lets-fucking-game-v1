a6439ecedfb0600dbcef2dfae43221093e276f2e
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2022-10-27 19:32:14 +0200

    Delete fromSeed.ts

--- packages/hardhat/helpers/fromSeed.ts
+++
@@ -1,7 +0,0 @@
-const ethers = require('ethers')
-
-require('dotenv').config()
-
-const mnemonicWallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-
-console.log('private key', mnemonicWallet.privateKey)
