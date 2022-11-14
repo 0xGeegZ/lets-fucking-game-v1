ea0893df8266d214d8d5e298e286e6619434751a
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2022-11-14 09:29:58 +0100

    feat: updating sentry config

--- packages/dapp/sentry.client.config.js
+++ packages/dapp/sentry.client.config.js
@@ -2,48 +2,16 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { init, GlobalHandlers, Breadcrumbs, Dedupe } from '@sentry/react'
+import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-const isUserRejected = (err) => {
-  // provider user rejected error code
-  return typeof err === 'object' && 'code' in err && err.code === 4001
-}
-
-const ENV = process.env.VERCEL_ENV || process.env.NODE_ENV
-
-init({
-  dsn: SENTRY_DSN,
-  integrations: [
-    new Breadcrumbs({
-      console: ENV === 'production',
-    }),
-    new GlobalHandlers({
-      onerror: false,
-      onunhandledrejection: false,
-    }),
-    new Dedupe(),
-  ],
-  environment: ENV === 'production' ? 'production' : 'development',
+Sentry.init({
+  dsn: SENTRY_DSN || 'https://1ba54ac96ff34398a3cde5d3df46fae0@o307140.ingest.sentry.io/4504156524904448',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0,
+  tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  beforeSend(event, hint) {
-    const error = hint?.originalException
-    if (error && isUserRejected(error)) {
-      return null
-    }
-    return event
-  },
-  ignoreErrors: [
-    'User denied transaction signature',
-    'Non-Error promise rejection captured',
-    'User rejected the transaction',
-    'cancelled',
-    'PollingBlockTracker',
-  ],
-})
+});
