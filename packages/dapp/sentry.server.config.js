3b0bfec9437ca542e7404c8a29d1ccb96da688d2
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2022-11-14 19:38:32 +0100

    feat: updating sentry config

--- packages/dapp/sentry.server.config.js
+++ packages/dapp/sentry.server.config.js
@@ -2,16 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import { init } from '@sentry/nextjs'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+const ENV = process.env.VERCEL_ENV || process.env.NODE_ENV
 
-Sentry.init({
-  dsn: SENTRY_DSN || 'https://1ba54ac96ff34398a3cde5d3df46fae0@o307140.ingest.sentry.io/4504156524904448',
+init({
+  dsn: SENTRY_DSN,
+  environment: ENV === 'production' ? 'production' : 'development',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-});
+})
