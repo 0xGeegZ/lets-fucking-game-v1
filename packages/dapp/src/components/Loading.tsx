f5256f61b8e528091826b028a14f6508a9cacc6d
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2022-11-27 00:07:27 +0100

    feat: updating loader component

--- packages/dapp/src/components/Loading.tsx
+++ packages/dapp/src/components/Loading.tsx
@@ -1,33 +1,5 @@
 import styled, { keyframes } from 'styled-components'
 
-// const Loading = styled.div`
-//   border: 8px solid #f3f3f3;
-//   border-radius: 50%;
-//   border-top: 8px solid #ddd;
-//   border-bottom: 8px solid #ddd;
-//   width: 20px;
-//   height: 20px;
-//   -webkit-animation: spin 2s linear infinite;
-//   animation: spin 2s linear infinite;
-//   @-webkit-keyframes spin {
-//     0% {
-//       -webkit-transform: rotate(0deg);
-//     }
-//     100% {
-//       -webkit-transform: rotate(360deg);
-//     }
-//   }
-
-//   @keyframes spin {
-//     0% {
-//       transform: rotate(0deg);
-//     }
-//     100% {
-//       transform: rotate(360deg);
-//     }
-//   }
-// `
-
 const rotate360 = keyframes`
   from {
     transform: rotate(0deg);
@@ -41,9 +13,9 @@ const Loading = styled.div`
   animation: ${rotate360} 1s linear infinite;
   transform: translateZ(0);
 
-  border-top: 2px solid grey;
-  border-right: 2px solid grey;
-  border-bottom: 2px solid grey;
+  border-top: 3px solid grey;
+  border-right: 3px solid grey;
+  border-bottom: 3px solid grey;
   border-left: 4px solid black;
   background: transparent;
   width: 24px;
