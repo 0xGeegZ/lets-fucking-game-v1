99632da781c99a7fca3ad03718dc9ab655b7ab6d
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2022-11-26 16:41:00 +0100

    feat: cleaning dapp config folder with backup

---
+++ packages/dapp/src/_config/constants/meta.ts
@@ -0,0 +1,45 @@
+import memoize from 'lodash/memoize'
+import { ContextApi } from '@pancakeswap/localization'
+import { PageMeta } from './types'
+
+export const DEFAULT_META: PageMeta = {
+  title: "Let's F*#@$* Game",
+  description:
+    'Let\'s Fucking Game allows you to create "one button games" and offer them to your twitter community in order to provide them with a fun way to engage with your content. Once the game starts. players have to log in everyday during a random time slot. If you forget, you lose. The last players share the prizes according to the prizepool distribution.',
+  image: 'https://avatars.githubusercontent.com/u/115870339?s=200&v=4',
+}
+
+interface PathList {
+  paths: { [path: string]: { title: string; basePath?: boolean; description?: string } }
+  defaultTitleSuffix: string
+}
+
+const getPathList = (t: ContextApi['t']): PathList => {
+  return {
+    paths: {
+      '/': { title: t('Home') },
+      '/games': { title: t('Games') },
+      '/game': { title: t('Game') },
+      '/create-game': { title: t('Create Game') },
+    },
+    defaultTitleSuffix: t("Let's F*#@$* Game"),
+  }
+}
+
+export const getCustomMeta = memoize(
+  (path: string, t: ContextApi['t'], _: string): PageMeta => {
+    const pathList = getPathList(t)
+    const pathMetadata =
+      pathList.paths[path] ??
+      pathList.paths[Object.entries(pathList.paths).find(([url, data]) => data.basePath && path.startsWith(url))?.[0]]
+
+    if (pathMetadata) {
+      return {
+        title: `${pathMetadata.title} | ${t(pathList.defaultTitleSuffix)}`,
+        ...(pathMetadata.description && { description: pathMetadata.description }),
+      }
+    }
+    return null
+  },
+  (path, t, locale) => `${path}#${locale}`,
+)
---
+++ packages/dapp/src/_config/internal/gameConfig.ts
@@ -0,0 +1,112 @@
+// GAME CONFIG : UPDATE DEFAULT CONFIG FROM HARDHAT PACKAGE THEN COPY CHANGE TO DAPP BY COMPILING OR DEPLOYING CONTRACT
+import { BigNumber } from '@ethersproject/bignumber'
+import { parseEther } from '@ethersproject/units'
+
+export const range = (start, end) => Array.from(Array(end + 1).keys()).slice(start)
+
+const randomNumber = () => {
+  return Math.floor(Math.random() * (10000 - 1) + 1)
+}
+
+export interface GameConfig {
+  NAME_DEFAULT: string
+  NAME_MIN_LENGTH: number
+  NAME_MAX_LENGTH: number
+
+  PLAYERS_DEFAULT: number
+  PLAYERS_MIN_LENGTH: number
+  PLAYERS_MAX_LENGTH: number
+
+  GAME_CREATION_AMOUNT: BigNumber
+
+  PLAY_TIME_RANGE_DEFAULT: number
+  AUTHORIZED_PLAY_TIME_RANGE: Array<number>
+
+  REGISTRATION_AMOUNT_DEFAULT: BigNumber
+  REGISTRATION_AMOUNT_FREE: BigNumber
+  AUTHORIZED_REGISTRATION_AMOUNTS: Array<number>
+
+  PRIZEPOOL_NUMBER: number
+  PRIZEPOOL_AMOUNT: BigNumber
+
+  ENCODED_CRON_DEFAULT: string
+  AUTHORIZED_CRONS: Array<number>
+
+  PRIZETYPE: Array<string>
+
+  TREASURY_FEE_DEFAULT: number
+  TREASURY_FEE_MIN: number
+  TREASURY_FEE_MAX: number
+  AUTHORIZED_TREASURY_FEE: Array<number>
+  CREATOR_FEE_DEFAULT: number
+  CREATOR_FEE_MIN: number
+  CREATOR_FEE_MAX: number
+  AUTHORIZED_CREATOR_FEE: Array<number>
+}
+export const defaultGameConfig: GameConfig = {
+  NAME_DEFAULT: `LFG MVP #${randomNumber()}`,
+  NAME_MIN_LENGTH: 3,
+  NAME_MAX_LENGTH: 32,
+
+  PLAYERS_DEFAULT: 10,
+  PLAYERS_MIN_LENGTH: 2,
+  PLAYERS_MAX_LENGTH: 100,
+
+  GAME_CREATION_AMOUNT: parseEther('0.01'),
+
+  PLAY_TIME_RANGE_DEFAULT: 2,
+  AUTHORIZED_PLAY_TIME_RANGE: [...range(1, 5)],
+
+  REGISTRATION_AMOUNT_DEFAULT: parseEther('0.0001'),
+  REGISTRATION_AMOUNT_FREE: parseEther('0'),
+  AUTHORIZED_REGISTRATION_AMOUNTS: [0, 0.0001, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 5, 10],
+
+  PRIZEPOOL_NUMBER: 0.01,
+  PRIZEPOOL_AMOUNT: parseEther('0.01'),
+
+  ENCODED_CRON_DEFAULT: '0 17 * * *',
+  AUTHORIZED_CRONS: [...range(0, 23)],
+
+  PRIZETYPE: ['ERC20', 'ERC721', 'ERC1155'],
+
+  TREASURY_FEE_DEFAULT: 100,
+  TREASURY_FEE_MIN: 0,
+  TREASURY_FEE_MAX: 100,
+  AUTHORIZED_TREASURY_FEE: [...range(3, 10)],
+  CREATOR_FEE_DEFAULT: 300,
+  CREATOR_FEE_MIN: 0,
+  CREATOR_FEE_MAX: 500,
+  AUTHORIZED_CREATOR_FEE: [...range(0, 5)],
+}
+
+export const gameConfig: Record<string, GameConfig | null> = {
+  '31337': { ...defaultGameConfig },
+  '1337': { ...defaultGameConfig },
+  '56': {
+    ...defaultGameConfig,
+    GAME_CREATION_AMOUNT: parseEther('0.5'),
+    AUTHORIZED_REGISTRATION_AMOUNTS: [0, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 5, 10],
+    REGISTRATION_AMOUNT_DEFAULT: parseEther('0.1'),
+  },
+  '97': {
+    ...defaultGameConfig,
+    GAME_CREATION_AMOUNT: parseEther('0.1'),
+    // GAME_CREATION_AMOUNT: parseEther('0.2'),
+    PLAYERS_DEFAULT: 5,
+  },
+  '5': {
+    ...defaultGameConfig,
+    GAME_CREATION_AMOUNT: parseEther('0.05'),
+    // GAME_CREATION_AMOUNT: parseEther('0.05'),
+    PLAYERS_DEFAULT: 5,
+  },
+  '80001': {
+    ...defaultGameConfig,
+    GAME_CREATION_AMOUNT: parseEther('0.01'),
+    // GAME_CREATION_AMOUNT: parseEther('50'),
+    PLAYERS_DEFAULT: 5,
+    AUTHORIZED_REGISTRATION_AMOUNTS: [0, 0.0001, 0.5, 1, 2, 5, 10, 100, 200, 250],
+  },
+}
+
+export const developmentChains = ['hardhat', 'localhost']
