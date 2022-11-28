486dc1d1f64abe6455faf05da35bac680087a9b8
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2022-11-28 21:42:10 +0100

    feat: updating loader

--- packages/dapp/src/components/Loader/PageLoader.tsx
+++ packages/dapp/src/components/Loader/PageLoader.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Spinner } from '@pancakeswap/uikit'
+import Loading from 'components/Loading'
 import Page from '../Layout/Page'
 
 const Wrapper = styled(Page)`
@@ -11,7 +11,7 @@ const Wrapper = styled(Page)`
 const PageLoader: React.FC<React.PropsWithChildren<unknown>> = () => {
   return (
     <Wrapper>
-      <Spinner />
+      <Loading />
     </Wrapper>
   )
 }
